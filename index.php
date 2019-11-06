69d627142f6bcb8517acc7dcc5ac56f4365d0f15
lukeconsultoria <57047990+lukeconsultoria@users.noreply.github.com>
2019-11-06 15:02:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+echo "olá mundo";
+
+$nome = "hcode";
+
+echo $nome. "mais alguma coisa";
+
+$nome.= "treinamentos ";
+
+echo $nome;
+
+$valortotal = 0 ;
+
+$valortotal += 3.000;
+
+$valortotal += 0;
+
+$valortotal *= .4;
+
+
+echo $valortotal;
+
+
+
+$a = 10000000000;
+
+$b = 2;
+
+echo $a + $b;
+
+echo "</br>";
+
+echo $a * $b;
+
+echo "</br>";
+
+echo $a - $b;
+
+echo "</br>";
+
+echo $a / $b;
+
+echo "</br>";
+
+echo $a % $b ;
+*/
+
+$a = 38;
+
+$b = 55;
+
+var_dump($a > $b);
+
+var_dump($a < $b);
+
+
+
+
+
+
+
+
+
+
+
+?>
